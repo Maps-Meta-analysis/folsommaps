a107454240c1e3bfb536a39cddc0f6abde544247
Darvesh Gorhe <dgorhe123@gmail.com>
2023-02-25 18:56:07 -0800

    style: renamed file from app.js to index.js

---
+++ index.js
@@ -0,0 +1,59 @@
+/* global document */
+import * as React from 'react';
+import {createRoot} from 'react-dom/client';
+import Map, {Source, Layer} from 'react-map-gl';
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoiZGdvcmhlIiwiYSI6ImNsZWo1YmcxdjA2cHYzcG52OHlybzZqMnUifQ.YcADDs9WU7XpOFxcNNlK7Q'; // Set your mapbox token here
+let link = "https://raw.githubusercontent.com/dgorhe/folsommaps/main/geojson/folsom-border.geojson"
+
+const layerStyle = {
+    id: 'border',
+    type: 'line',
+    paint: {
+        'line-width': 5,
+        'line-color': '#007cbf'
+    }
+};
+
+function GeoComponent({link}) {
+    const [data, setData] = React.useState(null);
+    React.useEffect(() => {
+        fetch(link)
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, [link]);
+    if (!data) {
+        return null;
+    }
+    return (
+        <Source type="geojson" data={data}>
+            <Layer {...layerStyle} />
+        </Source>
+    );
+}
+
+function Root() {
+    return (
+        <Map
+        initialViewState={{
+            latitude: 38.6780,
+            longitude: -121.1581,
+            zoom: 11.5
+        }}
+        style={{width: "100vw", height: "100vh"}}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxAccessToken={MAPBOX_TOKEN}
+        >
+        <GeoComponent link = {link}/>
+        </Map>
+    );
+}
+
+const root = createRoot(
+    document.body.appendChild(
+        document.createElement('div')
+    )
+);
+root.render(<Root/>);
