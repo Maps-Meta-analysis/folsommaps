4090de69c6c10d682c72492a7c3ddeb183ab3531
Darvesh Gorhe <darveshgorhe@Darveshs-MacBook-Pro.local>
2023-03-23 17:28:56 -0700

    feat: token is requested from firebase instead of being stored statically in the app

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 /* global document */
 import * as React from 'react';
+import {useState, useEffect} from 'react';
 import {createRoot} from 'react-dom/client';
 import Map, {Source, Layer} from 'react-map-gl';
-
+import './index.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN;
 let link = "https://raw.githubusercontent.com/dgorhe/folsommaps/main/geojson/folsom-border.geojson";
+let BASE = "https://us-central1-folsommaps.cloudfunctions.net/";
 
 const layerStyle = {
     id: 'border',
@@ -17,16 +18,32 @@ const layerStyle = {
     }
 };
 
+function HelloWorld( {} ) {
+    const [helloWorld, setHelloWorld] = useState(null);
+
+    useEffect(() => {
+        fetch(BASE + 'readMapboxToken')
+            .then(response => (response.json()))
+            .then(data => setHelloWorld(data["token"]))
+    }, []);
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <p>{helloWorld}</p>
+        </div>
+    );
+}
+
 function GeoComponent({link}) {
     const [data, setData] = React.useState(null);
+    
     React.useEffect(() => {
         fetch(link)
             .then(response => response.json())
             .then(data => setData(data));
     }, [link]);
-    if (!data) {
-        return null;
-    }
+    
     return (
         <Source type="geojson" data={data}>
             <Layer {...layerStyle} />
@@ -34,20 +51,40 @@ function GeoComponent({link}) {
     );
 }
 
+function MapComponent({link}) {
+    const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        fetch(BASE + 'readMapboxToken')
+            .then(response => response.json())
+            .then(data => setToken(data["token"]))
+
+        return () => {
+            console.log("Unmounting");
+        }
+    }, [token]);
+    
+    return token ? (
+        <Map
+            initialViewState={{
+                latitude: 38.6780,
+                longitude: -121.1581,
+                zoom: 11.5
+            }}
+            style={{width: "100vw", height: "100vh"}}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={token}
+        >
+            <GeoComponent link={link} />
+        </Map>
+    ) : (
+        <div>Loading map...</div>
+    );
+}
+
 function Root() {
     return (
-        <Map
-        initialViewState={{
-            latitude: 38.6780,
-            longitude: -121.1581,
-            zoom: 11.5
-        }}
-        style={{width: "100vw", height: "100vh"}}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={MAPBOX_TOKEN}
-        >
-        <GeoComponent link = {link}/>
-        </Map>
+        <MapComponent link = {link}></MapComponent>
     );
 }
 
