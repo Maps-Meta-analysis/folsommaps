792355c3694fb22e77fa50db5db1de20f539e380
Darvesh Gorhe <darveshgorhe@gmail.com>
2023-10-03 21:23:38 -0400

    style: added eslint to follow Google JavaScript style guide and edited files accordingly

--- control-panel.jsx
+++ control-panel.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import {useState} from 'react';
 import * as style from './style';
 // import { motion, AnimatePresence } from 'framer-motion';
 
@@ -8,10 +8,10 @@ const COLORS = {
   'BLUE': '#3E7EAE',
   'SKY BLUE': '#4EADC9',
   'GREEN': '#216331',
-  'Yellow': '#FBCE80'
-}
+  'Yellow': '#FBCE80',
+};
 
-function ControlPanel({ activeButtons, handleButtonChanged }) {
+function ControlPanel({activeButtons, handleButtonChanged}) {
   const [showPanel, setShowPanel] = useState(false);
 
   const togglePanel = () => {
@@ -19,14 +19,14 @@ function ControlPanel({ activeButtons, handleButtonChanged }) {
   };
 
   return (
-    <div className="control-panel" style={{ background: 'transparent', padding: '10px', width: '200px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <button onClick={togglePanel} className="control-panel-button" style={{ height: '40px', backgroundColor: COLORS['BLUE'], borderRadius: '20px', padding: '10px 20px', color: 'white', fontWeight: 'bold', fontSize: '16px' }}>
-        <span style={{ marginRight: '5px' }}>{showPanel ? '▼' : '►'}</span>
+    <div className="control-panel" style={{background: 'transparent', padding: '10px', width: '200px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <button onClick={togglePanel} className="control-panel-button" style={{height: '40px', backgroundColor: COLORS['BLUE'], borderRadius: '20px', padding: '10px 20px', color: 'white', fontWeight: 'bold', fontSize: '16px'}}>
+        <span style={{marginRight: '5px'}}>{showPanel ? '▼' : '►'}</span>
         Overlays
       </button>
-      
+
       {showPanel && (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
           <button
             id="overlay1"
             name="Voting Districts"
@@ -34,10 +34,10 @@ function ControlPanel({ activeButtons, handleButtonChanged }) {
             style={{
               ...style.buttonStyle,
               backgroundColor: COLORS['GREEN'],
-              opacity: activeButtons["Voting Districts"] ? 1 : 0.5
+              opacity: activeButtons['Voting Districts'] ? 1 : 0.5,
             }}
           >
-            <span style={{ ...style.overlayLabelStyling, color:"black", cursor: 'pointer' }}>Voting Districts</span>
+            <span style={{...style.overlayLabelStyling, color: 'black', cursor: 'pointer'}}>Voting Districts</span>
           </button>
 
           <button
@@ -47,10 +47,10 @@ function ControlPanel({ activeButtons, handleButtonChanged }) {
             style={{
               ...style.buttonStyle,
               backgroundColor: COLORS['GREEN'],
-              opacity: activeButtons["FCUSD Boundaries"] ? 1 : 0.5
+              opacity: activeButtons['FCUSD Boundaries'] ? 1 : 0.5,
             }}
           >
-            <span style={{ ...style.overlayLabelStyling, color:"black", cursor: 'pointer' }}>FCUSD Boundaries</span>
+            <span style={{...style.overlayLabelStyling, color: 'black', cursor: 'pointer'}}>FCUSD Boundaries</span>
           </button>
         </div>
       )}
--- index.js
+++ index.js
@@ -5,143 +5,186 @@ import {createRoot} from 'react-dom/client';
 import Map, {Source, Layer} from 'react-map-gl';
 import Loader from './loader';
 import ControlPanel from './control-panel';
-import './index.css'
+import './index.css';
 import * as style from './style';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import * as props from './propValidations';
 
-const branch = "toggle-data-elements";
-let BASE = `https://us-central1-folsommaps.cloudfunctions.net/`;
+const branch = 'toggle-data-elements';
+const BASE = `https://us-central1-folsommaps.cloudfunctions.net/`;
 
 const GEOJSON = {
-    'Folsom Border': `https://raw.githubusercontent.com/dgorhe/folsommaps/${branch}/geojson/folsom-border.geojson`,
-    'Voting Districts': `https://raw.githubusercontent.com/dgorhe/folsommaps/${branch}/geojson/test-voting-districts.geojson`,
-    'FCUSD Boundaries': `https://raw.githubusercontent.com/dgorhe/folsommaps/${branch}/geojson/test-fcusd-boundaries.geojson`,
-}
+  'Folsom Border': `https://raw.githubusercontent.com/dgorhe/folsommaps/${branch}/geojson/folsom-border.geojson`,
+  'Voting Districts': `https://raw.githubusercontent.com/dgorhe/folsommaps/${branch}/geojson/test-voting-districts.geojson`,
+  'FCUSD Boundaries': `https://raw.githubusercontent.com/dgorhe/folsommaps/${branch}/geojson/test-fcusd-boundaries.geojson`,
+};
 
-function GeoComponent({name, link}) {
-    const [data, setData] = React.useState(null);
-    
-    React.useEffect(() => {
-        if (link !== null) {
-            fetch(link)
-                .then(response => response.json())
-                .then(data => { setData(data);});
-        }
-    }, [link]);
+GeoComponent.propTypes = props.GeoComponentProps;
+MapComponent.propTypes = props.MapComponentProps;
+Root.propTypes = props.RootProps;
 
-    if (name == "Folsom Border") {
-        return (
-            <Source type="geojson" data={data}>
-                <Layer {...style.borderLayerStyle} />
-            </Source>
-        );
-    } else if (name == "Voting Districts") {
-        return (
-            <Source type="geojson" data={data}>
-                <Layer {...style.votingDistrictsLayerStyle} />
-            </Source>
-        );
-    } else if (name == "FCUSD Boundaries") {
-        return (
-            <Source type="geojson" data={data}>
-                <Layer {...style.fcusdBoundariesLayerStyle} />
-            </Source>
-        );
+/**
+ * A React component that renders a GeoJSON layer based on the given name.
+ * @param {Object} props - The props object.
+ * @param {string} props.name - The name of the GeoJSON layer to render.
+ * @return {JSX.Element} A JSX element that renders the GeoJSON layer.
+ */
+function GeoComponent({name}) {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    if (link !== null) {
+      fetch(link)
+          .then((response) => response.json())
+          .then((data) => {
+            setData(data);
+          });
     }
-}
+  }, [link]);
 
-function MapComponent({activeOverlays}) {
-    const [token, setToken] = useState(null);
-
-    useEffect(() => {
-        fetch(BASE + 'readMapboxToken')
-            .then(response => response.json())
-            .then(data => setToken(data["token"]))
-
-        return () => {
-            console.log("Unmounting");
-        }
-    }, [token]);
-    
-    if (token) {
-        return (
-            <Map
-                initialViewState={{
-                    latitude: 38.6780,
-                    longitude: -121.1581,
-                    zoom: 11.5
-                }}
-                style={{width: "100vw", height: "100vh"}}
-                mapStyle="mapbox://styles/mapbox/streets-v8"
-                mapboxAccessToken={token}
-            >
-                <GeoComponent name={'Folsom Border'} link={activeOverlays['Folsom Border']} />
-                {activeOverlays['Voting Districts'] && <GeoComponent name={'Voting Districts'} link={activeOverlays['Voting Districts']} />}
-                {activeOverlays['FCUSD Boundaries'] && <GeoComponent name={'FCUSD Boundaries'} link={activeOverlays['FCUSD Boundaries']} />}
-            </Map>
-        )
-    } else {
-        return (
-            <div
-              style={{
-                display: 'flex',
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh', // Adjust this value to control the height of the centered content
-              }}
-            >
-              <Loader />
-            </div>
-        );
-    }
-}
-
-function Root() {
-    const [activeOverlays, setActiveOverlays] = useState({
-        'Folsom Border': GEOJSON['Folsom Border'], 
-        'Voting Districts': null, 
-        'FCUSD Boundaries': null
-    });
-
-    const [activeButtons, setActiveButtons] = useState({
-        'Folsom Border': true,
-        'Voting Districts': false,
-        'FCUSD Boundaries': false
-    });
-
-    const handleButtonChanged = (event) => {
-        const { name } = event.currentTarget;
-    
-        // Update activeButtons based on current state
-        setActiveButtons(prevActiveButtons => {
-            const newState = { ...prevActiveButtons, [name]: !prevActiveButtons[name] };
-    
-            // Update activeOverlays based on the new activeButtons state
-            setActiveOverlays(prevActiveOverlays => {
-                return {
-                    ...prevActiveOverlays,
-                    [name]: newState[name] ? GEOJSON[name] : null
-                };
-            });
-    
-            return newState;
-        });
-    }
-    
-    
+  if (name == 'Folsom Border') {
     return (
-        <div className='map-container'>
-            <ControlPanel activeButtons={activeButtons} handleButtonChanged={handleButtonChanged} />
-            <MapComponent activeOverlays={activeOverlays}/>
-        </div>
+      <Source type="geojson" data={data}>
+        <Layer {...style.borderLayerStyle} />
+      </Source>
     );
+  } else if (name == 'Voting Districts') {
+    return (
+      <Source type="geojson" data={data}>
+        <Layer {...style.votingDistrictsLayerStyle} />
+      </Source>
+    );
+  } else if (name == 'FCUSD Boundaries') {
+    return (
+      <Source type="geojson" data={data}>
+        <Layer {...style.fcusdBoundariesLayerStyle} />
+      </Source>
+    );
+  }
+}
+
+/**
+ * Renders a Map component with GeoComponents based on the active overlays.
+ * @param {Object} props - The props object.
+ * @param {Object} props.activeOverlays - object with the active overlays.
+ * @return {JSX.Element} - A Map component with GeoComponents.
+ */
+function MapComponent({activeOverlays}) {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    fetch(BASE + 'readMapboxToken')
+        .then((response) => response.json())
+        .then((data) => setToken(data['token']));
+
+    return () => {
+      console.log('Unmounting');
+    };
+  }, [token]);
+
+  if (token) {
+    return (
+      <Map
+        initialViewState={{
+          latitude: 38.6780,
+          longitude: -121.1581,
+          zoom: 11.5,
+        }}
+        style={{width: '100vw', height: '100vh'}}
+        mapStyle="mapbox://styles/mapbox/streets-v8"
+        mapboxAccessToken={token}
+      >
+        <GeoComponent
+          name={'Folsom Border'}
+          link={activeOverlays['Folsom Border']}
+        />
+
+        {
+          activeOverlays['Voting Districts'] &&
+          <GeoComponent
+            name={'Voting Districts'}
+            link={activeOverlays['Voting Districts']
+            }/>
+        }
+        {
+          activeOverlays['FCUSD Boundaries'] &&
+          <GeoComponent
+            name={'FCUSD Boundaries'}
+            link={activeOverlays['FCUSD Boundaries']
+            }/>
+        }
+      </Map>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Loader />
+      </div>
+    );
+  }
+}
+
+/**
+ * Component that renders the map container, control panel and map component.
+ * @return {JSX.Element} The map container, control panel and map component.
+ */
+function Root() {
+  const [activeOverlays, setActiveOverlays] = useState({
+    'Folsom Border': GEOJSON['Folsom Border'],
+    'Voting Districts': null,
+    'FCUSD Boundaries': null,
+  });
+
+  const [activeButtons, setActiveButtons] = useState({
+    'Folsom Border': true,
+    'Voting Districts': false,
+    'FCUSD Boundaries': false,
+  });
+
+  const handleButtonChanged = (event) => {
+    const {name} = event.currentTarget;
+
+    // Update activeButtons based on current state
+    setActiveButtons((prevActiveButtons) => {
+      const newState = {...prevActiveButtons, [name]: !prevActiveButtons[name]};
+
+      // Update activeOverlays based on the new activeButtons state
+      setActiveOverlays((prevActiveOverlays) => {
+        return {
+          ...prevActiveOverlays,
+          [name]: newState[name] ? GEOJSON[name] : null,
+        };
+      });
+
+      return newState;
+    });
+  };
+
+
+  return (
+    <div className='map-container'>
+      <ControlPanel
+        activeButtons={activeButtons}
+        handleButtonChanged={handleButtonChanged}
+      />
+
+      <MapComponent activeOverlays={activeOverlays}/>
+    </div>
+  );
 }
 
 const root = createRoot(
     document.body.appendChild(
-        document.createElement('div')
-    )
+        document.createElement('div'),
+    ),
 );
 
 root.render(<Root/>);
